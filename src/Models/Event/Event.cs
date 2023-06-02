0157cf44e7231001dafa51d97e49d355036e21d5
Heitor Soares <heitor_soares@live.com>
2023-06-02 15:00:18 -0300

    first-commit

---
+++ src/Models/Event/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace SChallenge.Models.Event
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string? EventName { get; set; }
+        public int? NumberParticipans { get; set; }
+        public DateTime? EventDate { get; set; }
+        public int? EventDuration { get; set; }
+        public string? Creator { get; set; }
+    }
+}
