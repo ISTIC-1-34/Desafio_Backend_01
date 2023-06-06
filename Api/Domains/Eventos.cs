a2bd26f4be6e0806c8a544b489f44c8294865f5c
Julia Bergman <55153493+biribrinha@users.noreply.github.com>
2023-06-06 10:57:22 -0300

    Add files via upload

---
+++ Api/Domains/Eventos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Api.Domains
+{
+    public partial class Eventos
+    {
+
+        public int Id { get; set; }
+        public string NomeEvento { get; set; }
+        public int QuantidadeParticipantes { get; set; }
+        public string DataEvento { get; set; }
+        public string DuracaoEvento { get; set; }
+        public string CriadorEvento { get; set; }
+
+    }
+}
